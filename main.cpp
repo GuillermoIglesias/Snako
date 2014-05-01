0aed3a70f4f2f091b134070f69d6aa61f9dfa9e2
GuillermoIglesias <guillermo.iglesias.90@gmail.com>
2014-04-30 20:39:15 -0400

    Snako v0.1
    - Removida función puertas temporalmente
    - Agregada colisión con murallas principales

--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@
 #include <curses.h>
 #include <windows.h>             //Incluye la funcion Sleep(), para el manejo de los segundos
 #include <time.h>                //Incluye la seed time(NULL) para numeros random
-#include <stdio.h>
 
-void murallas()
+void murallas(int vec[25][80])
 {
     int a, b;
 
@@ -15,33 +14,46 @@ void murallas()
     {
         mvaddch(a,1, ACS_BLOCK);
         mvaddch(a,78, ACS_BLOCK);
-        if (a==2||a==22)
-        {
-            for(b=1; b<=78; b++)
-                mvaddch(a,b, ACS_BLOCK);
-        }
+        vec[a][1]=1;
+        vec[a][78]=1;
     }
+    for(b=1; b<=78; b++)
+    {
+        mvaddch(2,b, ACS_BLOCK);
+        mvaddch(22,b, ACS_BLOCK);
+        vec[2][b]=1;
+        vec[22][b]=1;
+    }
+
     //Cuadro Nivel 1
     for(a=6; a<=18; a++)
     {
         mvaddch(a,5, ACS_BLOCK);
         mvaddch(a,74, ACS_BLOCK);
-        if (a==6||a==18)
-        {
-            for(b=5; b<=74; b++)
-                mvaddch(a,b, ACS_BLOCK);
-        }
+        vec[a][5]=1;
+        vec[a][74]=1;
+    }
+    for(b=5; b<=74; b++)
+    {
+        mvaddch(6,b, ACS_BLOCK);
+        mvaddch(18,b, ACS_BLOCK);
+        vec[6][b]=1;
+        vec[18][b]=1;
     }
     //Cuadro Nivel 2
     for(a=10; a<=14; a++)
     {
         mvaddch(a,9, ACS_BLOCK);
         mvaddch(a,70, ACS_BLOCK);
-        if (a==10||a==14)
-        {
-            for(b=9; b<=70; b++)
-                mvaddch(a,b, ACS_BLOCK);
-        }
+        vec[a][9]=1;
+        vec[a][70]=1;
+    }
+    for(b=9; b<=70; b++)
+    {
+        mvaddch(10,b, ACS_BLOCK);
+        mvaddch(14,b, ACS_BLOCK);
+        vec[10][b]=1;
+        vec[14][b]=1;
     }
 
     refresh();
@@ -58,81 +70,81 @@ void pasadizos(int vec[25][80])
     {
         pasadizo_sup=(rand()%62)+7;
         mvaddch(6,pasadizo_sup++, '1');
-        vec[6][pasadizo_sup]=1;
+        vec[6][pasadizo_sup]=2;
         mvaddch(6,pasadizo_sup++, '2');
-        vec[6][pasadizo_sup]=1;
+        vec[6][pasadizo_sup]=2;
         mvaddch(6,pasadizo_sup++, '3');
-        vec[6][pasadizo_sup]=1;
+        vec[6][pasadizo_sup]=2;
     }
     //Pasadizo Inferior
     for (i=1; i<4; i++)
     {
         pasadizo_inf=(rand()%62)+7;
         mvaddch(18,pasadizo_inf++, '1');
-        vec[18][pasadizo_inf]=1;
+        vec[18][pasadizo_inf]=2;
         mvaddch(18,pasadizo_inf++, '2');
-        vec[18][pasadizo_inf]=1;
+        vec[18][pasadizo_inf]=2;
         mvaddch(18,pasadizo_inf++, '3');
-        vec[18][pasadizo_inf]=1;
+        vec[18][pasadizo_inf]=2;
     }
     //Pasadizo Izquierda
     for (i=1; i<2; i++)
     {
         pasadizo_izq=(rand()%8)+7;
         mvaddch(pasadizo_izq++, 5, '1');
-        vec[pasadizo_izq][5]=1;
+        vec[pasadizo_izq][5]=2;
         mvaddch(pasadizo_izq++, 5, '2');
-        vec[pasadizo_izq][5]=1;
+        vec[pasadizo_izq][5]=2;
         mvaddch(pasadizo_izq++, 5, '3');
-        vec[pasadizo_izq][5]=1;
+        vec[pasadizo_izq][5]=2;
     }
     //Pasadizo Derecha
     for (i=1; i<2; i++)
     {
         pasadizo_der=(rand()%8)+7;
         mvaddch(pasadizo_der++, 74, '1');
-        vec[pasadizo_der][74]=1;
+        vec[pasadizo_der][74]=2;
         mvaddch(pasadizo_der++, 74, '2');
-        vec[pasadizo_der][74]=1;
+        vec[pasadizo_der][74]=2;
         mvaddch(pasadizo_der++, 74, '3');
-        vec[pasadizo_der][74]=1;
+        vec[pasadizo_der][74]=2;
     }
     //Pasadsizo Interior (Superior)
     for(i=1; i<2; i++)
     {
         interior_sup=(rand()%50)+13;
         mvaddch(10, interior_sup++, '1');
-        vec[10][interior_sup]=1;
+        vec[10][interior_sup]=2;
         mvaddch(10, interior_sup++, '2');
-        vec[10][interior_sup]=1;
+        vec[10][interior_sup]=2;
         mvaddch(10, interior_sup++, '3');
-        vec[10][interior_sup]=1;
+        vec[10][interior_sup]=2;
     }
     //Pasadizo Interior (Inferior)
     for(i=1; i<2; i++)
     {
         interior_inf=(rand()%50)+13;
         mvaddch(14, interior_inf++, '1');
-        vec[14][interior_inf]=1;
+        vec[14][interior_inf]=2;
         mvaddch(14, interior_inf++, '2');
-        vec[14][interior_inf]=1;
+        vec[14][interior_inf]=2;
         mvaddch(14, interior_inf++, '3');
-        vec[14][interior_inf]=1;
+        vec[14][interior_inf]=2;
     }
     refresh();
 }
 
 void puertas(int vec[25][80])
 {
-    int i, j, v;
+    int i;
     int puerta_sup1, puerta_inf1, puerta_izq1, puerta_der1;
-    int puerta_sup2, puerta_inf2, puerta_izq2, puerta_der2;
+    int puerta_sup2, puerta_inf2;
 
     //Puerta Superior Nivel 1
     for (i=1; i<=4; i++)
     {
         puerta_sup1=(rand()%67)+7;
-        if (vec[6][puerta_sup1]==0)
+        if (vec[6][puerta_sup1]!=2)
         {
             mvaddch(6,puerta_sup1, ACS_DIAMOND);
             mvaddch(5,puerta_sup1, ACS_DIAMOND);
@@ -146,7 +158,7 @@ void puertas(int vec[25][80])
     for (i=1; i<=4; i++)
     {
         puerta_inf1=(rand()%67)+7;
-        if (vec[18][puerta_inf1]==0)
+        if (vec[18][puerta_inf1]!=2)
         {
             mvaddch(18,puerta_inf1, ACS_DIAMOND);
             mvaddch(19,puerta_inf1, ACS_DIAMOND);
@@ -160,7 +172,7 @@ void puertas(int vec[25][80])
     for (i=1; i<=3; i++)
     {
         puerta_izq1=(rand()%8)+7;
-        if (vec[puerta_izq1][5]==0)
+        if (vec[puerta_izq1][5]!=2)
         {
             mvaddch(puerta_izq1, 5, ACS_DIAMOND);
             mvaddch(puerta_izq1, 4, ACS_DIAMOND);
@@ -174,7 +186,7 @@ void puertas(int vec[25][80])
     for (i=1; i<=3; i++)
     {
         puerta_der1=(rand()%8)+7;
-        if (vec[puerta_der1][74]==0)
+        if (vec[puerta_der1][74]!=2)
         {
             mvaddch(puerta_der1, 74, ACS_DIAMOND);
             mvaddch(puerta_der1, 75, ACS_DIAMOND);
@@ -188,7 +200,7 @@ void puertas(int vec[25][80])
     for (i=1; i<=2; i++)
     {
         puerta_sup2=(rand()%50)+13;
-        if (vec[10][puerta_sup2]==0)
+        if (vec[10][puerta_sup2]!=2)
         {
             mvaddch(10,puerta_sup2, ACS_DIAMOND);
             mvaddch(9,puerta_sup2, ACS_DIAMOND);
@@ -202,7 +214,7 @@ void puertas(int vec[25][80])
     for (i=1; i<=2; i++)
     {
         puerta_inf2=(rand()%50)+13;
-        if (vec[14][puerta_inf2]==0)
+        if (vec[14][puerta_inf2]!=2)
         {
             mvaddch(14,puerta_inf2, ACS_DIAMOND);
             mvaddch(15,puerta_inf2, ACS_DIAMOND);
@@ -215,11 +227,11 @@ void puertas(int vec[25][80])
     refresh();
 }
 
-void cabeza()
+void cabeza(int vec[25][80])
 {
     int x=3, y=20;
     int ch;
-    const int speed=300;        //Constante de velocidad indirectamente proporcional
+    const int speed=150;        //Constante de velocidad indirectamente proporcional
     bool movimiento=true;
     bool derecha=true;
     bool izquierda=false;
@@ -230,100 +242,109 @@ void cabeza()
     {
         while(derecha)
         {
-            mvaddch(y,x,'>');
-            Sleep(speed);
-            x++;
-            refresh();
-            ch=getch();
-            if (ch==KEY_UP)
+            if (vec[y][x]==0)
             {
-                derecha=false;
-                arriba=true;
-            }
-            else if (ch==KEY_DOWN)
-            {
-                derecha=false;
-                abajo=true;
-            }
-            else if (ch==KEY_LEFT)
-            {
-                derecha=false;
-                izquierda=true;
+                mvaddch(y,x,'>');
+                Sleep(speed);
+                x++;
+                refresh();
+                ch=getch();
+                if (ch==KEY_UP)
+                {
+                    derecha=false;
+                    arriba=true;
+                }
+                else if (ch==KEY_DOWN)
+                {
+                    derecha=false;
+                    abajo=true;
+                }
+                else if (ch==KEY_LEFT)
+                {
+                    derecha=false;
+                    izquierda=true;
+                }
             }
         }
 
         while(izquierda)
         {
-
-            mvaddch(y,x,'<');
-            Sleep(speed);
-            x--;
-            refresh();
-            ch=getch();
-            if (ch==KEY_UP)
+            if (vec[y][x]==0)
             {
-                izquierda=false;
-                arriba=true;
-            }
-            else if (ch==KEY_DOWN)
-            {
-                izquierda=false;
-                abajo=true;
-            }
-            else if (ch==KEY_RIGHT)
-            {
-                izquierda=false;
-                derecha=true;
+                mvaddch(y,x,'<');
+                Sleep(speed);
+                x--;
+                refresh();
+                ch=getch();
+                if (ch==KEY_UP)
+                {
+                    izquierda=false;
+                    arriba=true;
+                }
+                else if (ch==KEY_DOWN)
+                {
+                    izquierda=false;
+                    abajo=true;
+                }
+                else if (ch==KEY_RIGHT)
+                {
+                    izquierda=false;
+                    derecha=true;
+                }
             }
         }
 
         while(arriba)
         {
-
-            mvaddch(y,x,'^');
-            Sleep(speed);
-            y--;
-            refresh();
-            ch=getch();
-            if (ch==KEY_DOWN)
+            if (vec[y][x]==0)
             {
-                arriba=false;
-                abajo=true;
-            }
-            else if (ch==KEY_RIGHT)
-            {
-                arriba=false;
-                derecha=true;
-            }
-            else if (ch==KEY_LEFT)
-            {
-                arriba=false;
-                izquierda=true;
+                mvaddch(y,x,'^');
+                Sleep(speed);
+                y--;
+                refresh();
+                ch=getch();
+                if (ch==KEY_DOWN)
+                {
+                    arriba=false;
+                    abajo=true;
+                }
+                else if (ch==KEY_RIGHT)
+                {
+                    arriba=false;
+                    derecha=true;
+                }
+                else if (ch==KEY_LEFT)
+                {
+                    arriba=false;
+                    izquierda=true;
+                }
             }
         }
 
         while(abajo)
         {
-
-            mvaddch(y,x,'v');
-            Sleep(speed);
-            y++;
-            refresh();
-            ch=getch();
-            if (ch==KEY_UP)
+            if (vec[y][x]==0)
             {
-                abajo=false;
-                arriba=true;
-            }
-            else if (ch==KEY_RIGHT)
-            {
-                abajo=false;
-                derecha=true;
-            }
-            else if (ch==KEY_LEFT)
-            {
-                abajo=false;
-                izquierda=true;
+                mvaddch(y,x,'v');
+                Sleep(speed);
+                y++;
+                refresh();
+                ch=getch();
+                if (ch==KEY_UP)
+                {
+                    abajo=false;
+                    arriba=true;
+                }
+                else if (ch==KEY_RIGHT)
+                {
+                    abajo=false;
+                    derecha=true;
+                }
+                else if (ch==KEY_LEFT)
+                {
+                    abajo=false;
+                    izquierda=true;
+                }
             }
         }
     }
@@ -341,17 +362,16 @@ int main()
     nodelay(stdscr,1);              //Desactiva espera de lectura de teclado
     srand(time(NULL));              //Seed para rand()
 
-    for (int i=0; i<25; i++)      //Generador de Matriz para comprobaciones futuras
+    for (int i=0; i<25; i++)        //Generador de Matriz para comprobaciones futuras
         for (int j=0; j<80; j++)
             vec[i][j]=0;
 
 
-    murallas();
+    murallas(vec);
     pasadizos(vec);
-    puertas(vec);
+    /*puertas(vec);*/
+    cabeza(vec);                       //Funcion Cabeza
 
-    cabeza();                       //Funcion Cabeza
-
-    endwin();                       //Finaliza NCurses
+    endwin();                          //Finaliza NCurses
     return 0;
 }

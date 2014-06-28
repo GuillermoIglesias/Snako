7ba92b7b9ca21f3ab91484d52ddb8bba97be33f0
GuillermoIglesias <guillermo.iglesias.90@gmail.com>
2014-06-27 20:04:26 -0400

    Revert "Revert "Snako v0.5""
    This reverts commit ecd32887ba16a325b0720730242b222e3a0dc0c2.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,12 @@ void murallas(int vec[25][80])
 {
     int a, b;
 
+    mvaddstr (1,35, "S N A K O" );
+    mvaddstr (23,2, "Vidas:");
+    mvaddstr (1,2, "Puntaje: ");
+    mvaddstr (1,70, "Nivel");
+    mvaddstr (23,65, "Tiempo:");
+
     //Cuadro Nivel 0
     for(a=2; a<=22; a++)
     {
@@ -56,69 +62,111 @@ void pasadizos(int vec[25][80])
     //Pasadizo Superior
     for (i=1; i<4; i++)
     {
-        pasadizo_sup=(rand()%62)+7;
+        pasadizo_sup=(rand()%63)+8;
+
         vec[6][pasadizo_sup]=0;
-        mvaddch(6,pasadizo_sup++, ' ');
+        mvaddch(6,pasadizo_sup, ' ');
+
+        pasadizo_sup--;
+
         vec[6][pasadizo_sup]=0;
-        mvaddch(6,pasadizo_sup++, ' ');
+        mvaddch(6,pasadizo_sup, ' ');
+
+        pasadizo_sup += 2;
+
         vec[6][pasadizo_sup]=0;
-        mvaddch(6,pasadizo_sup++, ' ');
+        mvaddch(6,pasadizo_sup, ' ');
     }
     //Pasadizo Inferior
     for (i=1; i<4; i++)
     {
-        pasadizo_inf=(rand()%62)+7;
+        pasadizo_inf=(rand()%63)+8;
+
         vec[18][pasadizo_inf]=0;
-        mvaddch(18,pasadizo_inf++, ' ');
+        mvaddch(18,pasadizo_inf, ' ');
+
+        pasadizo_inf--;
+
         vec[18][pasadizo_inf]=0;
-        mvaddch(18,pasadizo_inf++, ' ');
+        mvaddch(18,pasadizo_inf, ' ');
+
+        pasadizo_inf += 2;
+
         vec[18][pasadizo_inf]=0;
-        mvaddch(18,pasadizo_inf++, ' ');
+        mvaddch(18,pasadizo_inf, ' ');
 
     }
     //Pasadizo Izquierda
     for (i=1; i<2; i++)
     {
-        pasadizo_izq=(rand()%8)+7;
+        pasadizo_izq=(rand()%9)+8;
+
         vec[pasadizo_izq][5]=0;
-        mvaddch(pasadizo_izq++, 5, ' ');
+        mvaddch(pasadizo_izq, 5, ' ');
+
+        pasadizo_izq--;
+
         vec[pasadizo_izq][5]=0;
-        mvaddch(pasadizo_izq++, 5, ' ');
+        mvaddch(pasadizo_izq, 5, ' ');
+
+        pasadizo_izq += 2;
+
         vec[pasadizo_izq][5]=0;
-        mvaddch(pasadizo_izq++, 5, ' ');
+        mvaddch(pasadizo_izq, 5, ' ');
     }
     //Pasadizo Derecha
     for (i=1; i<2; i++)
     {
-        pasadizo_der=(rand()%8)+7;
+        pasadizo_der=(rand()%9)+8;
+
         vec[pasadizo_der][74]=0;
-        mvaddch(pasadizo_der++, 74, ' ');
+        mvaddch(pasadizo_der, 74, ' ');
+
+        pasadizo_der--;
+
         vec[pasadizo_der][74]=0;
-        mvaddch(pasadizo_der++, 74, ' ');
+        mvaddch(pasadizo_der, 74, ' ');
+
+        pasadizo_der += 2;
+
         vec[pasadizo_der][74]=0;
-        mvaddch(pasadizo_der++, 74, ' ');
+        mvaddch(pasadizo_der, 74, ' ');
     }
     //Pasadsizo Interior (Superior)
     for(i=1; i<2; i++)
     {
-        interior_sup=(rand()%50)+13;
+        interior_sup=(rand()%58)+11;
+
         vec[10][interior_sup]=0;
-        mvaddch(10, interior_sup++, ' ');
+        mvaddch(10, interior_sup, ' ');
+
+        interior_sup--;
+
         vec[10][interior_sup]=0;
-        mvaddch(10, interior_sup++, ' ');
+        mvaddch(10, interior_sup, ' ');
+
+        interior_sup += 2;
+
         vec[10][interior_sup]=0;
-        mvaddch(10, interior_sup++, ' ');
+        mvaddch(10, interior_sup, ' ');
     }
     //Pasadizo Interior (Inferior)
     for(i=1; i<2; i++)
     {
-        interior_inf=(rand()%50)+13;
+        interior_inf=(rand()%58)+11;
+
         vec[14][interior_inf]=0;
-        mvaddch(14, interior_inf++, ' ');
+        mvaddch(14, interior_inf, ' ');
+
+        interior_inf--;
+
         vec[14][interior_inf]=0;
-        mvaddch(14, interior_inf++, ' ');
+        mvaddch(14, interior_inf, ' ');
+
+        interior_inf += 2;
+
         vec[14][interior_inf]=0;
-        mvaddch(14, interior_inf++, ' ');
+        mvaddch(14, interior_inf, ' ');
     }
     refresh();
 }
@@ -135,6 +183,7 @@ void puertas(int vec[25][80])
     {
         puerta_sup1=(rand()%67)+7;
         tipo = (rand()%3)+2;
+
         if (vec[6][puerta_sup1]!=0)
         {
             vec[5][puerta_sup1]=tipo;
@@ -147,8 +196,9 @@ void puertas(int vec[25][80])
     //Puerta Inferior Nivel 1
     for (i=1; i<=4; i++)
     {
-        puerta_inf1=(rand()%67)+7;
+        puerta_inf1=(rand()%64)+10;
         tipo = (rand()%3)+2;
+
         if (vec[18][puerta_inf1]!=0)
         {
             vec[19][puerta_inf1]=tipo;
@@ -159,10 +209,11 @@ void puertas(int vec[25][80])
             i--;
     }
     //Puerta Izquierda Nivel 1
-    for (i=1; i<=3; i++)
+    for (i=1; i<2; i++)
     {
-        puerta_izq1=(rand()%8)+7;
+        puerta_izq1=(rand()%6)+6;
         tipo = (rand()%3)+2;
+
         if (vec[puerta_izq1][5]!=0)
         {
             vec[puerta_izq1][4]=tipo;
@@ -173,10 +224,11 @@ void puertas(int vec[25][80])
             i--;
     }
     //Puerta Derecha Nivel 1
-    for (i=1; i<=3; i++)
+    for (i=1; i<2; i++)
     {
         puerta_der1=(rand()%8)+7;
         tipo = (rand()%3)+2;
+
         if (vec[puerta_der1][74]!=0)
         {
             vec[puerta_der1][75]=tipo;
@@ -191,6 +243,7 @@ void puertas(int vec[25][80])
     {
         puerta_sup2=(rand()%50)+13;
         tipo = (rand()%3)+2;
+
         if (vec[10][puerta_sup2]!=0&&vec[6][puerta_sup2]!=0)
         {
             vec[9][puerta_sup2]=tipo;
@@ -205,6 +258,7 @@ void puertas(int vec[25][80])
     {
         puerta_inf2=(rand()%50)+13;
         tipo = (rand()%3)+2;
+
         if (vec[14][puerta_inf2]!=0&&vec[18][puerta_inf2]!=0)
         {
             vec[15][puerta_inf2]=tipo;
@@ -214,13 +268,6 @@ void puertas(int vec[25][80])
         else
             i--;
     }
-    refresh();
-}
-
-void victoria(int vec[25][80])
-{
-    mvaddch(4,76, ACS_DIAMOND);
-    vec[4][76]=1;
     refresh();
 }
 
@@ -236,57 +283,63 @@ void imprimir_muralla(int vec[25][80])
 void imprimir_puerta(int vec[25][80], int &puerta)
 {
     //Imprime puertas tipo 2
-    if (puerta%5==0&&puerta%2!=0)
+    if (puerta%11==0&&puerta%2!=0)
         for (int i=0; i<=25; i++)
             for (int j=0; j<=80; j++)
-                if (vec[i][j]==2)
+                if (vec[i][j]==5)
                 {
                     mvaddch(i,j, ACS_BLOCK);
+                    vec[i][j]=2;
                 }
 
     //Elimina puertas tipo 2
-    if (puerta%10==0)
+    if (puerta%11==0&&puerta%2==0)
         for (int i=0; i<=25; i++)
             for (int j=0; j<=80; j++)
                 if (vec[i][j]==2)
                 {
                     mvaddch(i,j, ' ');
+                    vec[i][j]=5;
                 }
 
     //Imprime puertas tipo 3
-    if (puerta%3==0&&puerta%2!=0)
-        for (int i=0; i<=25; i++)
-            for (int j=0; j<=80; j++)
-                if (vec[i][j]==3)
-                {
-                    mvaddch(i,j, ACS_BLOCK);
-                }
-
-    //Elimina puertas tipo 3
-    if (puerta%6==0)
-        for (int i=0; i<=25; i++)
-            for (int j=0; j<=80; j++)
-                if (vec[i][j]==3)
-                {
-                    mvaddch(i,j, ' ');
-                }
-
-    //Imprime puertas tipo 4
     if (puerta%7==0&&puerta%2!=0)
         for (int i=0; i<=25; i++)
             for (int j=0; j<=80; j++)
-                if (vec[i][j]==4)
+                if (vec[i][j]==6)
                 {
                     mvaddch(i,j, ACS_BLOCK);
+                    vec[i][j]=3;
+                }
+
+    //Elimina puertas tipo 3
+    if (puerta%14==0)
+        for (int i=0; i<=25; i++)
+            for (int j=0; j<=80; j++)
+                if (vec[i][j]==3)
+                {
+                    mvaddch(i,j, ' ');
+                    vec[i][j]=6;
+                }
+
+    //Imprime puertas tipo 4
+    if (puerta%5==0&&puerta%2!=0)
+        for (int i=0; i<=25; i++)
+            for (int j=0; j<=80; j++)
+                if (vec[i][j]==7)
+                {
+                    mvaddch(i,j, ACS_BLOCK);
+                    vec[i][j]=4;
                 }
 
     //Elimina puertas tipo 4
-    if (puerta%14==0)
+    if (puerta%10==0)
         for (int i=0; i<=25; i++)
             for (int j=0; j<=80; j++)
                 if (vec[i][j]==4)
                 {
                     mvaddch(i,j, ' ');
+                    vec[i][j]=7;
                 }
 
     if (puerta > 9999)
@@ -295,7 +348,50 @@ void imprimir_puerta(int vec[25][80], int &puerta)
     refresh();
 }
 
-void cabeza(int vec[25][80])
+void comida(int vec[25][80])
+{
+    int posX, posY;
+
+    for (int i=0; i<2; i++)
+    {
+        posX = (rand()%78+1);
+        posY = (rand()%21+2);
+        if (vec[posY][posX]==0)
+        {
+            mvaddch(posY, posX, ACS_DIAMOND);
+            vec[posY][posX]=9;
+            i=2;
+        }
+        else
+            i--;
+    }
+
+    refresh();
+}
+
+void cola(int camino[25][80], int vec[25][80], int y, int x, int pasos, int &largo)
+{
+    if (vec[y][x]==9)
+    {
+        largo += 4;
+        vec[y][x] = 0;
+        comida(vec);
+    }
+
+    int arrastre = largo;
+
+    if (pasos>largo)
+    {
+        for (int i=0; i<=25; i++)
+            for (int j=0; j<=80; j++)
+                if (camino[i][j]== pasos - arrastre)
+                {
+                    mvaddch(i,j, ' ');
+                }
+    }
+}
+
+void cabeza(int vec[25][80], int camino[25][80])
 {
     int x=3, y=20;
     int ch;
@@ -307,18 +403,25 @@ void cabeza(int vec[25][80])
     bool arriba=false;
     int puerta = 0;
 
+    int pasos = 1;
+    int largo = 5;
+
     while (movimiento)
     {
         while(derecha)
         {
-            if (vec[y][x]==0)
+            if (vec[y][x]==0 || vec[y][x]==5 || vec[y][x]==6 || vec[y][x]==7 || vec[y][x]==9)
             {
                 halfdelay(speed);
                 imprimir_puerta(vec, puerta);
                 puerta++;
-                mvaddch(y,x, ' ');
-                x++;
                 mvaddch(y,x, '>');
+
+                camino[y][x] = pasos;
+                pasos++;
+                cola(camino, vec, y, x, pasos, largo);
+
+                x++;
                 refresh();
                 ch=getch();
                 if (ch==KEY_UP)
@@ -336,14 +439,18 @@ void cabeza(int vec[25][80])
 
         while(izquierda)
         {
-            if (vec[y][x]==0)
+            if (vec[y][x]==0 || vec[y][x]==5 || vec[y][x]==6 || vec[y][x]==7 || vec[y][x]==9)
             {
                 halfdelay(speed);
                 imprimir_puerta(vec, puerta);
                 puerta++;
-                mvaddch(y,x, ' ');
-                x--;
                 mvaddch(y,x, '<');
+
+                camino[y][x] = pasos;
+                pasos++;
+                cola(camino, vec, y, x, pasos, largo);
+
+                x--;
                 refresh();
                 ch=getch();
                 if (ch==KEY_UP)
@@ -361,14 +468,18 @@ void cabeza(int vec[25][80])
 
         while(arriba)
         {
-            if (vec[y][x]==0)
+            if (vec[y][x]==0 || vec[y][x]==5 || vec[y][x]==6 || vec[y][x]==7 || vec[y][x]==9)
             {
                 halfdelay(speed);
                 imprimir_puerta(vec, puerta);
                 puerta++;
-                mvaddch(y,x, ' ');
-                y--;
                 mvaddch(y,x, '^');
+
+                camino[y][x] = pasos;
+                pasos++;
+                cola(camino, vec, y, x, pasos, largo);
+
+                y--;
                 refresh();
                 ch=getch();
                 if (ch==KEY_RIGHT)
@@ -386,14 +497,18 @@ void cabeza(int vec[25][80])
 
         while(abajo)
         {
-            if (vec[y][x]==0)
+            if (vec[y][x]==0 || vec[y][x]==5 || vec[y][x]==6 || vec[y][x]==7 || vec[y][x]==9)
             {
                 halfdelay(speed);
                 imprimir_puerta(vec, puerta);
                 puerta++;
-                mvaddch(y,x, ' ');
-                y++;
                 mvaddch(y,x, 'v');
+
+                camino[y][x] = pasos;
+                pasos++;
+                cola(camino, vec, y, x, pasos, largo);
+
+                y++;
                 refresh();
                 ch=getch();
                 if (ch==KEY_RIGHT)
@@ -414,6 +529,7 @@ void cabeza(int vec[25][80])
 int main()
 {
     int vec [25][80]={0};           //Generador de Matriz para comprobaciones futuras
+    int camino [25][80]={0};        //Generador de Matriz para Movimiento Snako
 
     initscr();                      //Inicio NCurses
     keypad(stdscr,1);               //Habilita teclado
@@ -425,10 +541,11 @@ int main()
 
     murallas(vec);
     imprimir_muralla(vec);
+
     puertas(vec);
     pasadizos(vec);
-    victoria(vec);
-    cabeza(vec);
+    comida(vec);                      //Valor = 9
+    cabeza(vec, camino);
 
 
     endwin();                          //Finaliza NCurses
